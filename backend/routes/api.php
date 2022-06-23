c14f964869edbf876b295d89e47e0ee24d7ca08e
ssouzafs <ssouza.s@hotmail.com>
2022-06-23 09:42:25 -0300

    Subindo backend

---
+++ backend/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/users', 'index')->name('users.index');
+});
+
+
