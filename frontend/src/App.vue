202c221d2289b20ba280d1e6d1248295e37363b9
ssouzafs <ssouza.s@hotmail.com>
2022-06-24 17:00:04 -0300

    Aula de envio de eventos do componente FILHO para o componente PAI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,17 @@
   <Nav />
 
   <router-view></router-view>
+  <p>{{ counter.count }}</p>
+
+  <button @click="counter.count++">Add count</button>
+  <CountChild @increment="(value) => counter.count+= value"/>
 </template>
 
 <script setup>
+import { reactive } from "vue";
+
 import Nav from "@/components/Nav.vue";
+import CountChild from "@/components/CountChild.vue";
 
+const counter = reactive({ count: 0 });
 </script>
-
-<style scoped>
-</style>
---
+++ frontend/src/components/CountChild.vue
@@ -0,0 +1,11 @@
+<template>
+ <button @click="add">Incrementar Contador </button> 
+</template>
+
+<script setup>
+const emit = defineEmits(['increment'])
+
+function add() {
+    emit('increment', 10)
+}
+</script>
