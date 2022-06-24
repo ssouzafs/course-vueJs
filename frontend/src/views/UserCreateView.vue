816673ca798a24bdb22c3b0a567cce3c25f6f077
ssouzafs <ssouza.s@hotmail.com>
2022-06-24 11:37:58 -0300

    Finalizado aula sobre Propriedades Comnputadas

--- frontend/src/views/UserCreateView.vue
+++ frontend/src/views/UserCreateView.vue
@@ -1,82 +1,57 @@
 <template>
-  <div>
-    <form action="">
-      <input
-        type="text"
-        v-model="user.first_name"
-        placeholder="Primeiro nome"
-      />
-      <span v-if="errors['first_name']">{{ errors["first_name"][0] }}</span>
-      <br />
-
-      <input type="text" v-model="user.last_name" placeholder="Segundo nome" />
-      <span v-if="errors['last_name']">{{ errors["last_name"][0] }}</span
-      ><br />
-
-      <input type="text" v-model="user.password" placeholder="Senha" />
-      <span v-if="errors['password']">{{ errors["password"][0] }}</span
-      ><br />
-
-      <input type="text" v-model="user.email" placeholder="E-mail" />
-      <span v-if="errors['email']">{{ errors["email"][0] }}</span
-      ><br />
-
-      <button @click.prevent="createUser">Cadastrar</button>
-    </form>
-
-    <p>{{ fullName }}</p>
-  </div>
+  <ul>
+    <h3>Administradores</h3>
+    <template v-for="(user, index) in users.data" :key="index">
+      <li v-if="user.is_admin">{{ user.first_name }}</li>
+    </template>
+  </ul>
+  <hr />
+  <ul>
+    <h3>Usuários</h3>
+    <template v-for="(user, index) in usersNotAdmins" :key="index">
+      <li> {{ user.first_name }}</li>
+    </template>
+  </ul>
+  <hr>
+  <p> Propriedade Compudata: 
+    {{ countComputed }}
+  </p>
+  <p> function:
+    {{ countFunction() }}
+  </p>
+  <button @click="computedCount++">Add Computed</button> <button @click="functionCount++">Function Count</button>
+  <br>
 </template>
 
 <script setup>
+
 import { api } from "@/services/api.js";
-import { ref, computed } from "vue";
+import { reactive, computed, ref } from "vue";
 
-const user = ref({});
-const errors = ref([]);
+const users = reactive({data:[]});
+const computedCount = ref(0);
+const functionCount = ref(0);
 
-async function createUser() {
-  await api
-    .post("/api/users/store", user)
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((err) => {
-      if (err.response?.data) {
-        errors.value = err.response.data["errors"];
-      }
+const usersNotAdmins = computed(() => {
+  return users.data.filter((user) => user.is_admin === 0);
+});
 
-      console.log(errors.value);
-    });
-}
-
-const fullName = computed(()=> {
-    return user.first_name + ' ' + user.last_name
+const countComputed = computed(() => {
+  console.log('Chamando computed Count')
+  return  computedCount.value
 })
+
+function countFunction() {
+  console.log('Chamando Function Count')
+  return functionCount.value
+}
+
+api
+  .get("api/users")
+  .then((res) => {
+    users.data = res.data;
+  })
+  .catch((error) => {
+    console.log("Error: " + error.response.data);
+  });
 </script>
-<style scoped>
-div {
-  width: 100%;
-  padding: 10px;
-}
-
-input {
-  margin: 2px;
-  height: 20px;
-  padding: 5px;
-}
-
-span {
-  display: block;
-  color: brown;
-  font-size: 0.8rem;
-}
-button {
-  margin-top: 5px;
-  padding: 6px 12px;
-  background: rgba(49, 134, 95, 0.61);
-  color: white;
-  border: none;
-  border-radius: 3px;
-}
-</style>
