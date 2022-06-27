f4eef0c44090e940fab1322621d4b4a0cbb91761
ssouzafs <ssouza.s@hotmail.com>
2022-06-27 17:37:04 -0300

    Aula de pesquisa com paginação

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,5 @@
-<script setup>
-import _ from "lodash";
-import { ref } from "vue";
-
-// const search = ref('')
-// function search() {
-//   console.log('Teste')
-// }
-const search = _.debounce((value) => {
-  console.log(value.target.value);
-}, 1000);
-</script>
-
 <template>
   <router-view></router-view>
-  <hr />
-  <input type="text" placeholder="Search" @keyup="search" />
+  <!-- <hr />
+  <input type="text" placeholder="Search" @keyup="search" /> -->
 </template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,34 +1,98 @@
 <template>
-  <h2>Home</h2>
-  <hr />
-  <button @click="modal.open = !modal.open">Open modal</button>
-
-  <template v-if="modal.open">
-    <Modal>
-      <template #header>
-        <h4>Cabeçalho</h4>
+  <div class="m-3">
+    <input
+      type="text"
+      placeholder="Pesquisar"
+      v-model="userSearch"
+      @keyup="search"
+    />
+    <hr />
+    <p class="result" v-for="(user, index) in users.data" :key="index">
+      {{ user.first_name }} {{ user.last_name }}
+    </p>
+    <Pagination :data="users" @pagination-change-page="handleEventPagination">
+      <template #prev-nav>
+        <span>&lt; Anterior</span>
       </template>
-      <template #default>
-        <form action="">
-            <h4>Conteudo</h4>
-            <input type="text"><br>
-            <input type="text"><br>
-            <input type="text"><br>
-            <input type="text"><br>
-        </form>
+      <template #next-nav>
+        <span>Próximo &gt;</span>
       </template>
-      
-      <template #footer>
-        <h4>Rodapé</h4>
-      </template>
-    </Modal>
-  </template>
+    </Pagination>
+    <p class="not-found" v-if="userNotFound">Usuário não encontrado!</p>
+  </div>
 </template>
-
 <script setup>
-import { reactive } from "vue";
-import Modal from "@/components/Modal.vue";
+import { onMounted, ref, computed } from "vue";
+import _ from "lodash";
+import Pagination from "laravel-vue-pagination";
+import { api } from "@/services/api";
 
-const modal = reactive({ open: false });
+const users = ref([]);
+const userSearch = ref("");
+
+async function getData(page = 1) {
+  await api
+    .get("/api/users?page=" + Number(page))
+    .then((response) => {
+      users.value = response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+onMounted(() => {
+  getData();
+});
+async function searchUser(page = 1) {
+  await api
+    .get("/api/users/search?page=" + Number(page), {
+      params: {
+        user: userSearch.value,
+      },
+    })
+    .then((response) => {
+      if (!userSearch.value) {
+        getData();
+        return;
+      }
+      users.value = response.data;
+      console.log(users.value.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function handleEventPagination(page) {
+  return userSearch.value ? searchUser(page) : getData(page);
+}
+
+const search = _.debounce(async () => {
+  searchUser();
+}, 450);
+
+const userNotFound = computed(() => {
+  return users.value.data == 0;
+});
 </script>
 
+<style scoped>
+.not-found {
+  border: 1px solid rgb(236, 40, 89);
+  padding: 10px 5px;
+  border-radius: 3px;
+  opacity: 0.875;
+  color: rgb(236, 40, 89);
+}
+
+.result {
+  list-style: none;
+  padding: 10px 5px;
+  /* border-radius: 3px; */
+  opacity: 0.875;
+  margin: 10px 0;
+  color: black;
+  border-bottom: 1px solid gray;
+}
+</style>
