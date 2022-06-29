a74374f659edab3cef750515d73b694188ded1db
ssouzafs <ssouza.s@hotmail.com>
2022-06-29 14:26:20 -0300

    Aula de watcheffect

---
+++ watch-effect/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h2>App</h2>
+  <hr>
+  <div class="container">
+    <div class="p-3 bg-light shadow">
+      <div class="form-group mb-2">
+        <input type="text" class="form-control" placeholder="Nome..." v-model="user.firstName" />
+      </div>
+      <div class="form-group mb-2">
+        <input type="text" class="form-control" placeholder="Sobrenome..." v-model="lastName" />
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref, watchEffect } from 'vue';
+
+const firstName = ref('');
+const lastName = ref('');
+const user = ref({
+  firstName: '',
+  lastName: ''
+})
+
+const items = ref([
+  {
+    id: 1,
+    name: 'sergio'
+  },
+  {
+    id: 2,
+    name: 'Maria'
+  },
+])
+
+/** Tipos Primitivos */
+// watch([firstName, lastName], ([valueFirstName, valueLastName], [oldFirstName, oldLastName]) => {
+//   console.log('New Value: ' + valueLastName);
+//   console.log('Old Value: ' + oldLastName);
+// })
+
+/** Objetos */
+watchEffect(() => {
+  console.log('É carregado antes de carregar o DOM. Fica ouvindo tudo.')
+})
+</script>
