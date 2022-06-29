8b289a345c9f25f1a383e2ed4ab8d2ded255f9f7
ssouzafs <ssouza.s@hotmail.com>
2022-06-29 11:54:32 -0300

    Finalizado implementação de conversor de dólar

--- convertdolar/src/App.vue
+++ convertdolar/src/App.vue
@@ -4,11 +4,11 @@
   <div class="container">
     <div class="p-3 bg-light shadow">
       <div class="form-group mb-2">
-        <Currency type="text" class="form-control" placeholder="Valor..." v-model="inputDolar"
+        <p class="dollar">Dólar hoje: <span> {{ showDollarToday }}</span> </p>
+        <Currency type="text" class="form-control" placeholder="Informe o valor em dólar..." v-model="inputDollar"
           :options="{ currency: 'USD' }" />
       </div>
-      <p>Dólar hoje: </p>
-      <p>Valor em reais: {{ dolarFormatedBrl }}</p>
+      <p class="dollar-in-real">{{ showDollarToReal }}</p>
     </div>
   </div>
 </template>
@@ -18,29 +18,56 @@ import { ref, watch, computed, onMounted } from 'vue';
 import { formatter } from '@/services/formatter';
 import { api } from '@/services/api';
 import Currency from '@/components/Currency';
-const inputDolar = ref(0);
-const dolarToday = ref(0);
+const inputDollar = ref(0);
+const grossDollar = ref(0);
+const dollarToReal = ref(0);
 
-const dolarFormatedBrl = computed(() => {
-  return formatter(inputDolar.value, 'en-US', 'USD');
+const showDollarToReal = computed(() => {
+  return formatter(dollarToReal.value, 'pt-BR', 'BRL');
 });
 
-const dolarTodayValue = computed(() => {
-
+const showDollarToday = computed(() => {
+  return formatter(grossDollar.value, 'en-US', 'USD');
 })
 
-onMounted(() => {
-  getCurrentDolar();
+onMounted(async () => {
+  try {
+    const rawCurrency = await getCurrentDolar();
+    grossDollar.value = rawCurrency.high;
+  } catch (err) {
+    console.log("Error: " + err.response.data);
+  }
 })
 
 async function getCurrentDolar(currencyTpe = 'USD-BRL') {
-  await api.get(`https://economia.awesomeapi.com.br/json/last/${currencyTpe}`)
-    .then((response) => {
-      const currencySplited = currencyTpe.split('-').join('');
-      return response.data[currencySplited];
-    }).catch((error) => {
-      console.log(error.response.data)
-    });
+  const { data } = await api.get(`https://economia.awesomeapi.com.br/json/last/${currencyTpe}`);
+  const currencySplited = currencyTpe.split('-').join('');
+  return data[currencySplited];
 }
 
+watch(inputDollar, (value) => {
+  dollarToReal.value = value * Number(grossDollar.value);
+})
 </script>
+
+<style scoped>
+
+input {
+   font-weight: 300;
+}
+.dollar-in-real {
+  padding: 8px;
+  border-radius: 3px;
+  color: rgb(47, 168, 128);
+  border: 1px solid rgb(47, 168, 128);
+  text-align: center;
+}
+.dollar {
+  font-weight: 300;
+}
+
+.dollar span {
+  color: rgb(235, 32, 116);
+  font-weight: 400;
+}
+</style>
